d2b54b7d93aedd3e99dac53fe176247dfb5332f5
daguiheso <daguiheso@gmail.com>
2017-12-29 22:31:38 -0500

    Intro testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+// Los unit-test comienzan con la palabra describe y lo que estanos probando, en este caso AppComponent
 describe('AppComponent', () => {
+  // funcion que se ejecuta antes de correr cada unit test
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,20 +11,25 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  // tests
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    // que sea verdadero y/o exista
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    // que titulo sea igual a app
     expect(app.title).toEqual('app');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
+    // que tenga ese contenido dentro de un h1
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
